c3c69d765cb40096c99391de70e5ae5aad492da8
Jessica Pichardo <jess.pichardo18@gmail.com>
2024-04-21 22:44:42 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+{const num1 = 62;
+const num2 = 1;
 
-// Write your code here
+const multiply = num1 * num2;
+}
+
+{const random = Math.floor(Math.random() * 100) + 1;
+}
+
+{const num3 = 18;
+    const num4 = 7;
+    
+    const mod = num3 % num4;
+}
+{const numbers = [10, 5, 20, 15, 7];
+    const max = Math.max(...numbers);
+}
